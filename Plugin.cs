e66cc30a7b289817c82cadfe1ffe8595c7017d57
Cadyn Zursturensatz <cadynspacetechguy@gmail.com>
2025-03-29 19:29:09 -0700

    Various fixes and additions

--- Plugin.cs
+++ Plugin.cs
@@ -5,18 +5,24 @@ using MenuLib.MonoBehaviors;
 using MenuLib;
 using UnityEngine;
 using Photon.Pun;
+using System.Collections.Generic;
 using Steamworks;
+using BepInEx.Configuration;
+using System.Linq;
 
 namespace UpgradeEveryRound;
 
-[BepInPlugin(modGUID, modName, modVersion), BepInDependency("nickklmao.menulib", "2.1.1")]
+[BepInPlugin(modGUID, modName, modVersion), BepInDependency("nickklmao.menulib", "2.1.3")]
 public class Plugin : BaseUnityPlugin
 {
     public const string modGUID = "dev.redfops.repo.upgradeeveryround";
-    public const string modName = "upgradeeveryround";
-    public const string modVersion = "1.0.0";
+    public const string modName = "Upgrade Every Round";
+    public const string modVersion = "1.1.0";
 
     public static bool isOpen = false;
+    public static ConfigEntry<int> upgradesPerRound;
+    public static ConfigEntry<bool> limitedChoices;
+    public static ConfigEntry<int> numChoices;
 
     internal static new ManualLogSource Logger;
     private readonly Harmony harmony = new Harmony(modGUID);
@@ -26,10 +32,16 @@ public class Plugin : BaseUnityPlugin
         // Plugin startup logic
         Logger = base.Logger;
 
+        upgradesPerRound = Config.Bind("Upgrades", "Upgrades Per Round", 1, new ConfigDescription("Number of upgrades per round", new AcceptableValueRange<int>(0, 10)));
+        limitedChoices = Config.Bind("Upgrades", "Limited random choices", false, new ConfigDescription("Only presents a fixed number of random options"));
+        numChoices = Config.Bind("Upgrades", "Number of choices", 3, new ConfigDescription("Number of options to choose from per upgrade", new AcceptableValueRange<int>(1, 8)));
+
+
+
         harmony.PatchAll(typeof(PlayerSpawnPatch));
-        harmony.PatchAll(typeof(MenuClosePatch));
-        harmony.PatchAll(typeof(MenuManagerClosePatch));
-        harmony.PatchAll(typeof(MenuManagerOpenPatch));
+        harmony.PatchAll(typeof(RunManagerChangeLevelPatch));
+        harmony.PatchAll(typeof(RunManagerMainMenuPatch));
+        harmony.PatchAll(typeof(StatsManagerPatch));
         harmony.PatchAll(typeof(UpgradeMapPlayerCountPatch));
         harmony.PatchAll(typeof(UpgradePlayerEnergyPatch));
         harmony.PatchAll(typeof(UpgradePlayerExtraJumpPatch));
@@ -42,6 +54,30 @@ public class Plugin : BaseUnityPlugin
 
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
+
+    public static void ApplyUpgrade(string _steamID, REPOPopupPage popupPage)
+    {
+        //Update UI to reflect upgrade
+        StatsUI.instance.Fetch();
+        StatsUI.instance.ShowStats();
+        CameraGlitch.Instance.PlayUpgrade();
+
+        int value = ++StatsManager.instance.dictionaryOfDictionaries["playerUpgradesUsed"][_steamID];
+        if (GameManager.Multiplayer())
+        {
+            //Broadcast that we used an upgrade
+            PhotonView _photonView = PunManager.instance.GetComponent<PhotonView>();
+            _photonView.RPC("UpdateStatRPC", RpcTarget.Others, "playerUpgradesUsed", _steamID, value);
+        }
+
+        //See if we are done upgrading, and if so close the menu
+        int upgradesDeserved = RunManager.instance.levelsCompleted * Plugin.upgradesPerRound.Value;
+        if (value >= upgradesDeserved)
+        {
+            isOpen = false;
+            popupPage.ClosePage(true);
+        }
+    }
 }
 
 [HarmonyPatch(typeof(PlayerAvatar))]
@@ -50,109 +86,124 @@ public static class PlayerSpawnPatch
 {
     static void Prefix(PhotonView ___photonView)
     {
-        if (RunManager.instance.levelShop != RunManager.instance.levelCurrent) return;
+        Level[] bannedLevels = [RunManager.instance.levelMainMenu, RunManager.instance.levelLobbyMenu, RunManager.instance.levelTutorial];
+        if (bannedLevels.Contains(RunManager.instance.levelCurrent)) return;
+
+        string _steamID = SemiFunc.PlayerGetSteamID(SemiFunc.PlayerAvatarGetFromPhotonID(SemiFunc.PhotonViewIDPlayerAvatarLocal()));
+        int upgradesDeserved = RunManager.instance.levelsCompleted * Plugin.upgradesPerRound.Value;
+
+#if DEBUG
+        upgradesDeserved += 1;
+#endif
+        if (StatsManager.instance.dictionaryOfDictionaries["playerUpgradesUsed"][_steamID] >= upgradesDeserved) return;
         if (GameManager.Multiplayer() && !___photonView.IsMine) return;
 
-        Plugin.isOpen = true;
-        var repoPopupPage = MenuAPI.CreateREPOPopupPage("Choose an upgrade", REPOPopupPage.PresetSide.Left, pageDimmerVisibility: true, spacing: 1.5f);
+        MenuManager.instance.PageCloseAll(); //Just in case somehow other menus were opened previously.
         
-        repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Stamina", () => {
-            PunManager.instance.UpgradePlayerEnergy(SemiFunc.PlayerGetSteamID(SemiFunc.PlayerAvatarGetFromPhotonID(SemiFunc.PhotonViewIDPlayerAvatarLocal())));
-            StatsUI.instance.Fetch();
-            StatsUI.instance.ShowStats();
-            CameraGlitch.Instance.PlayUpgrade();
+        var repoPopupPage = MenuAPI.CreateREPOPopupPage("Choose an upgrade", REPOPopupPage.PresetSide.Left, shouldCachePage: false, pageDimmerVisibility: true, spacing: 1.5f);
+
+        repoPopupPage.menuPage.onPageEnd.AddListener(() => { Plugin.isOpen = false; }); //They really shouldn't be able to close it, but just in case we want to make sure their menus work
+
+        int numChoices = Plugin.limitedChoices.Value ? Plugin.numChoices.Value : 8;
+        List<int> choices = [0, 1, 2, 3, 4, 5, 6, 7];
+
+        //Add limited buttons randomly or all in order depending on config
+        for (int i = 0; i < numChoices; i++)
+        {
+            int choiceIndex = Plugin.limitedChoices.Value ? Random.Range(0, choices.Count) : 0; //If not limited choices then we don't need to use random
+            int choice = choices[choiceIndex];
+            choices.RemoveAt(choiceIndex);
             
 
-            repoPopupPage.ClosePage(true);
-            Plugin.isOpen = false;
-            return;
-        }, parent, new Vector2(46f, 18f)));
-        
-        repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Extra Jump", () => {
-            PunManager.instance.UpgradePlayerExtraJump(SemiFunc.PlayerGetSteamID(SemiFunc.PlayerAvatarGetFromPhotonID(SemiFunc.PhotonViewIDPlayerAvatarLocal())));
-            StatsUI.instance.Fetch();
-            StatsUI.instance.ShowStats();
-            CameraGlitch.Instance.PlayUpgrade();
+            switch (choice)
+            {
+                case 0:
+                    repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Stamina", () =>
+                    {
+                        PunManager.instance.UpgradePlayerEnergy(_steamID);
+                        Plugin.ApplyUpgrade(_steamID, repoPopupPage);
+                        return;
+                    }, parent, new Vector2(46f, 18f)));
+                    break;
 
+                case 1:
+                    repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Extra Jump", () =>
+                    {
+                        PunManager.instance.UpgradePlayerExtraJump(_steamID);
+                        Plugin.ApplyUpgrade(_steamID, repoPopupPage);
+                        return;
+                    }, parent, new Vector2(186f, 18f)));
+                    break;
 
-            repoPopupPage.ClosePage(true);
-            Plugin.isOpen = false;
-            return;
-        }, parent, new Vector2(186f, 18f)));
+                case 2:
+                    repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Range", () =>
+                    {
+                        PunManager.instance.UpgradePlayerGrabRange(_steamID);
+                        Plugin.ApplyUpgrade(_steamID, repoPopupPage);
+                        return;
+                    }, parent, new Vector2(46f, 60f)));
+                    break;
 
-        repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Range", () => {
-            PunManager.instance.UpgradePlayerGrabRange(SemiFunc.PlayerGetSteamID(SemiFunc.PlayerAvatarGetFromPhotonID(SemiFunc.PhotonViewIDPlayerAvatarLocal())));
-            StatsUI.instance.Fetch();
-            StatsUI.instance.ShowStats();
-            CameraGlitch.Instance.PlayUpgrade();
+                case 3:
+                    repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Strength", () =>
+                    {
+                        PunManager.instance.UpgradePlayerGrabStrength(_steamID);
+                        Plugin.ApplyUpgrade(_steamID, repoPopupPage);
+                        return;
+                    }, parent, new Vector2(186f, 60f)));
+                    break;
 
+                case 4:
+                    repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Health", () =>
+                    {
+                        PunManager.instance.UpgradePlayerHealth(_steamID);
+                        Plugin.ApplyUpgrade(_steamID, repoPopupPage);
+                        return;
+                    }, parent, new Vector2(46f, 102f)));
+                    break;
 
-            repoPopupPage.ClosePage(true);
-            Plugin.isOpen = false;
-            return;
-        }, parent, new Vector2(46f, 60f)));
+                case 5:
+                    repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Sprint speed", () =>
+                    {
+                        PunManager.instance.UpgradePlayerSprintSpeed(_steamID);
+                        Plugin.ApplyUpgrade(_steamID, repoPopupPage);
+                        return;
+                    }, parent, new Vector2(186f, 102f)));
+                    break;
 
-        repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Strength", () => {
-            PunManager.instance.UpgradePlayerGrabStrength(SemiFunc.PlayerGetSteamID(SemiFunc.PlayerAvatarGetFromPhotonID(SemiFunc.PhotonViewIDPlayerAvatarLocal())));
-            StatsUI.instance.Fetch();
-            StatsUI.instance.ShowStats();
-            CameraGlitch.Instance.PlayUpgrade();
+                case 6:
+                    repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Tumble Launch", () =>
+                    {
+                        PunManager.instance.UpgradePlayerTumbleLaunch(_steamID);
+                        Plugin.ApplyUpgrade(_steamID, repoPopupPage);
+                        return;
+                    }, parent, new Vector2(46f, 144f)));
+                    break;
 
-
-            repoPopupPage.ClosePage(true);
-            Plugin.isOpen = false;
-            return;
-        }, parent, new Vector2(186f, 60f)));
-
-        repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Health", () => {
-            PunManager.instance.UpgradePlayerHealth(SemiFunc.PlayerGetSteamID(SemiFunc.PlayerAvatarGetFromPhotonID(SemiFunc.PhotonViewIDPlayerAvatarLocal())));
-            StatsUI.instance.Fetch();
-            StatsUI.instance.ShowStats();
-            CameraGlitch.Instance.PlayUpgrade();
-
-
-            repoPopupPage.ClosePage(true);
-            Plugin.isOpen = false;
-            return;
-        }, parent, new Vector2(46f, 102f)));
-
-        repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Sprint speed", () => {
-            PunManager.instance.UpgradePlayerSprintSpeed(SemiFunc.PlayerGetSteamID(SemiFunc.PlayerAvatarGetFromPhotonID(SemiFunc.PhotonViewIDPlayerAvatarLocal())));
-            StatsUI.instance.Fetch();
-            StatsUI.instance.ShowStats();
-            CameraGlitch.Instance.PlayUpgrade();
-
-
-            repoPopupPage.ClosePage(true);
-            Plugin.isOpen = false;
-            return;
-        }, parent, new Vector2(186f, 102f)));
-
-        repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Tumble Launch", () => {
-            PunManager.instance.UpgradePlayerTumbleLaunch(SemiFunc.PlayerGetSteamID(SemiFunc.PlayerAvatarGetFromPhotonID(SemiFunc.PhotonViewIDPlayerAvatarLocal())));
-            StatsUI.instance.Fetch();
-            StatsUI.instance.ShowStats();
-            CameraGlitch.Instance.PlayUpgrade();
-
-
-            repoPopupPage.ClosePage(true);
-            Plugin.isOpen = false;
-            return;
-        }, parent, new Vector2(46f, 144f)));
-
-        repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Map Player Count", () => {
-            PunManager.instance.UpgradeMapPlayerCount(SemiFunc.PlayerGetSteamID(SemiFunc.PlayerAvatarGetFromPhotonID(SemiFunc.PhotonViewIDPlayerAvatarLocal())));
-            StatsUI.instance.Fetch();
-            StatsUI.instance.ShowStats();
-            CameraGlitch.Instance.PlayUpgrade();
-
-
-            repoPopupPage.ClosePage(true);
-            Plugin.isOpen = false;
-            return;
-        }, parent, new Vector2(186f, 144f)));
+                case 7:
+                    repoPopupPage.AddElement(parent => MenuAPI.CreateREPOButton("Map Player Count", () =>
+                    {
+                        PunManager.instance.UpgradeMapPlayerCount(_steamID);
+                        Plugin.ApplyUpgrade(_steamID, repoPopupPage);
+                        return;
+                    }, parent, new Vector2(186f, 144f)));
+                    break;
+            }
+        }
 
         repoPopupPage.OpenPage(false);
+        Plugin.isOpen = true;
+    }
+}
+
+//Our custom save data handling
+[HarmonyPatch(typeof(StatsManager))]
+[HarmonyPatch("Start")]
+public static class StatsManagerPatch
+{
+    static void Prefix(StatsManager __instance)
+    {
+        __instance.dictionaryOfDictionaries.Add("playerUpgradesUsed",[]);
     }
 }
 
@@ -262,36 +313,23 @@ public static class UpgradePlayerTumbleLaunchPatch
     }
 }
 
-//All of this is horrible bad jankness to prevent players from accidentally closing the menu before they pick an upgrade
-
-[HarmonyPatch(typeof(MenuPagePopUp))]
-[HarmonyPatch(nameof(MenuPagePopUp.ButtonEvent))]
-public static class MenuClosePatch
+//So it turns out that things break sometimes, make sure we reset this value incase they escape the menu through other means
+[HarmonyPatch(typeof(RunManager))]
+[HarmonyPatch(nameof(RunManager.ChangeLevel))]
+public static class RunManagerChangeLevelPatch
 {
-    static bool Prefix()
+    static void Prefix()
     {
-        return !Plugin.isOpen;
+        Plugin.isOpen = false;
     }
 }
 
-
-
-[HarmonyPatch(typeof(MenuManager))]
-[HarmonyPatch(nameof(MenuManager.PageOpen))]
-public static class MenuManagerOpenPatch
+[HarmonyPatch(typeof(RunManager))]
+[HarmonyPatch(nameof(RunManager.LeaveToMainMenu))]
+public static class RunManagerMainMenuPatch
 {
-    static bool Prefix()
+    static void Prefix()
     {
-        return !Plugin.isOpen;
-    }
-}
-
-[HarmonyPatch(typeof(MenuManager))]
-[HarmonyPatch(nameof(MenuManager.PageCloseAll))]
-public static class MenuManagerClosePatch
-{
-    static bool Prefix()
-    {
-        return !Plugin.isOpen;
+        Plugin.isOpen = false;
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # UpgradeEveryRound
 Simple mod meant for larger groups that ensures everybody gets at least one upgrade per round.
 Players are prompted to choose which upgrade they want upon loading into the shop level.
+
+## Config
+### Upgrades Per Round
+Pretty self explanatory, this is the number of upgrades that each player gets every round
+### Limited Random Choices
+This a toggle which if set to true will make it so that each player has to choose from a randomly selected list of upgrades instead of being able to choose any upgrade
+### Number of Choices
+How many choices the player has available to choose from if Limited Random Choices is enabled
